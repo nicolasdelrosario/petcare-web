1612dc091791f0a780da118f299e03db39a7703e
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-12 13:04:23 -0500

    feat: :sparkles: add max width wrapper component

---
+++ components/max-width-wrapper/index.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+
+type MaxWidthWrapperProps = {
+	className?: string;
+	children: React.ReactNode;
+};
+
+function MaxWidthWrapper({ className, children }: MaxWidthWrapperProps) {
+	return (
+		<div
+			className={cn(
+				"mx-auto flex h-full w-full max-w-screen-xl flex-col px-4 sm:px-6 lg:px-8 ",
+				className,
+			)}
+		>
+			{children}
+		</div>
+	);
+}
+
+export default MaxWidthWrapper;
