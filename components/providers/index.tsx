f86bf14afe3a16d171cb7154b1ab51820527ff0b
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-12 13:07:59 -0500

    feat: :sparkles: add providers

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -1,9 +1,28 @@
+import QueryClientProvider from "@/components/providers/query-client-provider";
+import ReactScan from "@/components/providers/react-scan";
+import { Toaster } from "@/components/ui/sonner";
+import { env } from "@/config/env";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 type ProvidersProps = {
 	children: React.ReactNode;
 };
 
 function Providers({ children }: ProvidersProps) {
-	return <div>{children}</div>;
+	return (
+		<>
+			<QueryClientProvider>
+				{env.NODE_ENV === "development" && (
+					<>
+						<ReactQueryDevtools />
+						<ReactScan />
+					</>
+				)}
+				<Toaster />
+				{children}
+			</QueryClientProvider>
+		</>
+	);
 }
 
 export default Providers;
