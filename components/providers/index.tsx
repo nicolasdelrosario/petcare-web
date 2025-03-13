a553b6b08aa1d222d71b17e051b2d52bce45b1c9
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-13 14:49:42 -0500

    feat: :sparkles: implement theme provider to providers

--- components/providers/index.tsx
+++ components/providers/index.tsx
@@ -3,6 +3,7 @@ import ReactScan from "@/components/providers/react-scan";
 import { Toaster } from "@/components/ui/sonner";
 import { env } from "@/config/env";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ThemeProvider } from "./theme-provider";
 
 type ProvidersProps = {
 	children: React.ReactNode;
@@ -12,14 +13,21 @@ function Providers({ children }: ProvidersProps) {
 	return (
 		<>
 			<QueryClientProvider>
-				{env.NODE_ENV === "development" && (
-					<>
-						<ReactQueryDevtools />
-						<ReactScan />
-					</>
-				)}
-				<Toaster />
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{env.NODE_ENV === "development" && (
+						<>
+							<ReactQueryDevtools />
+							<ReactScan />
+						</>
+					)}
+					<Toaster />
+					{children}
+				</ThemeProvider>
 			</QueryClientProvider>
 		</>
 	);
