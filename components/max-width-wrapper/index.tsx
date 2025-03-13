be410aa0f6c7c7110e86071746a32eabcc655e51
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-13 13:17:36 -0500

    refactor: :lipstick: update ui

--- components/max-width-wrapper/index.tsx
+++ components/max-width-wrapper/index.tsx
@@ -9,7 +9,7 @@ function MaxWidthWrapper({ className, children }: MaxWidthWrapperProps) {
 	return (
 		<div
 			className={cn(
-				"mx-auto flex h-full w-full max-w-screen-xl flex-col px-4 sm:px-6 lg:px-8 ",
+				"mx-auto flex h-full w-full max-w-screen-xl flex-1 flex-col px-4 sm:px-6 lg:px-8 ",
 				className,
 			)}
 		>
