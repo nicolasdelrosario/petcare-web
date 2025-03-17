497ddd6559e1dc16601c00b9b40bb64db33ccafa
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-17 17:22:51 -0500

    refactor: :recycle: change update to patch

--- config/api.ts
+++ config/api.ts
@@ -8,14 +8,14 @@ export const apiRoutes = {
 		list: "/owners",
 		create: "/owners",
 		getOne: (id: number) => `/owners/${id}`,
-		update: (id: number) => `/owners/${id}`,
+		patch: (id: number) => `/owners/${id}`,
 		delete: (id: number) => `/owners/${id}`,
 	},
 	users: {
 		list: "/users",
 		create: "/users",
 		getOne: (id: number) => `/users/${id}`,
-		update: (id: number) => `/users/${id}`,
+		patch: (id: number) => `/users/${id}`,
 		delete: (id: number) => `/users/${id}`,
 	},
 } as const;
