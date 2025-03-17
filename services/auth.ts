d84d4c981071bfad58f5842e59d7af069b0ace12
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-16 23:29:59 -0500

    feat: :sparkles: add get me method in auth services

--- services/auth.ts
+++ services/auth.ts
@@ -1,5 +1,6 @@
 import { apiRoutes } from "@/config/api";
 import type { SignInSchema } from "@/schemas/auth";
+import type { UserWithWorkspaceSchema } from "@/schemas/user";
 import { api } from "@/services/api";
 
 export const authService = {
@@ -11,5 +12,10 @@ export const authService = {
 	async signOut() {
 		const { data } = await api.post(apiRoutes.auth.signOut);
 		return data;
+	},
+
+	async getMe() {
+		const { data } = await api.get(apiRoutes.auth.getMe);
+		return data as UserWithWorkspaceSchema;
 	},
 };
