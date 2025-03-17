7525934d16e2634fa20fcd16ad736fb2fe8845a4
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-03-16 23:11:04 -0500

    feat: :sparkles: add users and workspaces messages

--- config/messages.ts
+++ config/messages.ts
@@ -22,6 +22,20 @@ export const messages = {
 			invalidEmail: "Correo electrónico no válido",
 			invalidPassword: "La contraseña debe tener al menos 6 caracteres",
 		},
+		users: {
+			invalidName: "El nombre debe tener al menos 3 caracteres",
+			invalidEmail: "Correo electrónico no válido",
+			invalidWorkspace: "El espacio de trabajo es requerido",
+			invalidWorkspaceId: "El ID del espacio de trabajo no es válido",
+			invalidPhone: "El número de teléfono no es válido",
+			invalidRole: "Rol inválido",
+		},
+		workspaces: {
+			invalidName: "El nombre debe tener al menos 3 caracteres",
+			invalidSlug: "El slug debe tener al menos 3 caracteres",
+			descriptionTooShort: "La descripción debe tener al menos 10 caracteres",
+			descriptionTooLong: "La descripción debe tener menos de 500 caracteres",
+		},
 	},
 	success: {
 		auth: {
